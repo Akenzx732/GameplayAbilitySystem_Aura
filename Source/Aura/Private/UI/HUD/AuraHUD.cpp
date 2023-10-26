2e5b971a5fa3f379a96345a6e34dc6c1157f80d8
Anthony Kenny <Anthony.J.Kenny@outlook.com>
2023-10-26 18:07:01 -0500

    Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,15 @@
+// Copyright Anthony Kenny LLC
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "Blueprint/UserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+	
+}
