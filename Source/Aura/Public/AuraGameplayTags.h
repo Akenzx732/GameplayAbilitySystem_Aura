5bf4dc8c9c7f1bb9f5a38d80db8a0a6837d3921c
Anthony Kenny <Anthony.J.Kenny@outlook.com>
2023-11-07 23:42:04 -0600

    Aura Asset Manager

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,25 @@
+// Copyright Anthony Kenny LLC
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * Aura GameplayTags
+ *
+ * Singleton containing native Gameplay Tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+     static const FAuraGameplayTags& Get() {return GameplayTags;}
+     static void InitializeNativeGameplayTags();
+
+ FGameplayTag Attributes_Secondary_Armor;
+protected:
+
+private:
+     static FAuraGameplayTags GameplayTags;
+};
